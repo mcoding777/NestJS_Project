ad9c1a235b634210f292c19983f1ed6cff41d395
misun <mcoding777@naver.com>
2023-01-26 15:51:46 +0900

    chore: 기능별 설명 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
-@Controller('boards')
+// controller : Endpoint와 Method에 따라 콜백 함수 실행
+@Controller('boards') // 엔드포인트
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+// service : 해당 Endpoint와 관련된 함수 정리
+@Injectable() // @Injectable : 해당 service를 어느 컨트롤러에서든 사용할 수 있게 해주는 데코레이터
 export class BoardsService {
   private boards = [];
 
