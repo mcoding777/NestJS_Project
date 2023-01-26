9325b0517481a35f7f7ad6e59096b019b9d9a9d2
misun <mcoding777@naver.com>
2023-01-26 15:51:01 +0900

    chore: 기능별 설명 추가

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
+// module : controller와 provider 등록
 @Module({
   controllers: [BoardsController],
   providers: [BoardsService],
