ecae34f422ec28da15eb8033c26664d51ce07a15
misun <mcoding777@naver.com>
2023-02-15 11:31:58 +0900

    feat: 새로운 엔드포인트 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { UserModule } from './user/user.module';
+import { TargetModule } from './target/target.module';
 
 @Module({
-  imports: [BoardsModule, UserModule],
+  imports: [BoardsModule, UserModule, TargetModule],
 })
 export class AppModule {}
