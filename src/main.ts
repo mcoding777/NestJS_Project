6c70001fa72ab45a7e24e2c0b8cd0690cfbda3ba
misun <mcoding777@naver.com>
2023-02-23 16:34:35 +0900

    feat: user DTO 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // CORS 허용
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // dto에 없는 값은 거르고 에러 출력
+      forbidNonWhitelisted: true, // dto에 없는 값이 들어오면 에러 출력
+      transform: true, // string이 넘어와서 타입 변경을 해야하는 불편함을 없애줌
+    }),
+  );
   await app.listen(7777);
 }
 bootstrap();
